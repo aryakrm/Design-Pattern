50d9e797655fce936ced79669033ff5dcd5167f9
Arya Karimi <devaryakrm@gmail.com>
2023-01-14 15:05:25 +0300

    code added

---
+++ decorate.mjs
@@ -0,0 +1,12 @@
+class Shout extends Text {
+  constructor(text) {
+    super(text);
+  }
+
+  toString() {
+    return this.string.toUpperCase();
+  }
+}
+
+console.log(new Text("Hello, I'm talking").toString());
+console.log(new Shout(new Text("Hello, I'm shouting!")).toString());
